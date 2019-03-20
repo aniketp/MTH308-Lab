513e68420f69edd4dd391c2b0d2483ca5c05158d
Aniket Pandey <aniketp@iitk.ac.in>
2019-03-20 13:21:08 +0530

    Switch to Modified Steepest-Descent and Newton-Raphson

--- Mini-Project/main.m
+++ Mini-Project/main.m
@@ -1,10 +1,10 @@
 function main()
     % the curve
-    X    = @(t) 2*cos(2*pi*t);
-    Y    = @(t) 3*sin(2*pi*t);
-    dXdt = @(t) -4*pi*sin(2*pi*t);
-    dYdt = @(t) 6*pi*cos(2*pi*t);
-    % point to be projected
+    X    = @(t) sin(2*pi*t) + 3*sin(4*pi*t);
+    Y    = @(t) 2*cos(2*pi*t) + 5*cos(24*pi*t);
+    dXdt = @(t) 2*pi*cos(2*pi*t) + 12*pi*cos(4*pi*t);
+    dYdt = @(t) -4*pi*sin(2*pi*t) - 120*pi*sin(24*pi*t);
+	% point to be projected
     x0  = input('x0 = ');
     y0  = input('y0 = ');
     % desired accuracy
--- Mini-Project/orthoProjectionOnCurve.m
+++ Mini-Project/orthoProjectionOnCurve.m
@@ -1,39 +1,50 @@
 % Orthogonal projection on a curve for a given point
 %
 % Function signature: returns the parameter t
+%
+% Name: Aniket Pandey
+% Roll: 160113
 
 function t0 = orthoProjectionOnCurve(x0, y0, X, Y, dXdt, dYdt, eps)
-    a = eps/5;		% This needs to be checked for a possible optimisation
-    % f(t) = (distance)^2
+    DEBUG = false;
+
+    % f(t) = (distance)^2 (to be minimised)
     dist = @(t) (X(t) - x0)^2 + (Y(t) - y0)^2;
 
     % Since the period of the given parametric curve is 1, the relevant
     % range of values of t is [0,1)
-    % Take initial value of t0 and compute the corresponding distance.
-    % We then find the value of t0 which gives the least distance.
+    % Divide the curve in 200 sections and find the one with minimum
+    % distance. That is the starting point for next step.
     t0 = 0.0;
-    d = dist(0.0);
-    for t_iter = 0.1: 0.1: 0.9
-        temp = dist(t_iter);
-        if (temp < d)
+    d_min = dist(t0);
+    for t_iter = 0.0: 0.005: 0.995
+        d_cur = dist(t_iter);
+        if (d_cur < d_min)
             t0 = t_iter;
-            d = temp;
+            d_min = d_cur;
         end
     end
+    if DEBUG
+        fprintf("DEBUG: Min Distance of all sections is: %f\n", d_min);
+    end
 
-    x_prime = @(t) 2*(X(t) - x0)*dXdt(t);
-    y_prime = @(t) 2*(Y(t) - y0)*dYdt(t);
-    grad_f =  @(t) x_prime(t) + y_prime(t);
+    A = @(t) [X(t) - x0, Y(t) - y0];
+    B = @(t) [dXdt(t), dYdt(t)];
+    cosine = @(t) dot(A(t), B(t))/ (norm(A(t), 2) * norm(B(t), 2));
 
-    % Stop when difference between consecutive "t" is within 0.0001
-    % We limit the number of iterations to be 50 as the initial value
-    % of t was chosen fairly close to the solution, and hence 50
-    % iterations is a
+    % Core of the modified Steepest Descent Algorithm and Newton-Raphson method
+    % Stop when the absolute value of cosine is contained within the provided
+    % error (epsilon). To avoid an infinite loop in case of a malicious input or
+    % a badly conditioned function, we restrict the number of iterations to 5000.
     count = 0;
-
-    while ( abs(grad_f(t0)) > dist(eps) && count < 50)
-        t0 = t0 - a*grad_f(t0);
+    while (abs(cosine(t0)) > eps && count < 5000)
+        t0 = t0 - (eps/2)*cosine(t0);
         count = count + 1;
     end
 
+    if DEBUG
+        fprintf("DEBUG:\n\tcosine: %i\n\teps: %i\n\tcount: %i\n\tdist: %i\n\n",...
+            cosine(t0), eps, count, dist(t0));
+    end
 end
+
