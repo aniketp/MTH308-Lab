a7f65edb4343993fb50f510a36116a87d274a1a8
Aniket Pandey <aniketp@iitk.ac.in>
2019-03-29 21:33:42 +0530

    Use the expected accuracy margin for the solution

--- Mini-Project/main.m
+++ Mini-Project/main.m
@@ -4,11 +4,12 @@ function main()
     Y    = @(t) 2*cos(2*pi*t) + 5*cos(24*pi*t);
     dXdt = @(t) 2*pi*cos(2*pi*t) + 12*pi*cos(4*pi*t);
     dYdt = @(t) -4*pi*sin(2*pi*t) - 120*pi*sin(24*pi*t);
-	% point to be projected
+    % point to be projected
     x0  = input('x0 = ');
     y0  = input('y0 = ');
     % desired accuracy
-    eps = input('eps = ');
+    eps = power(10, -14);
+
     tic;
     tc = orthoProjectionOnCurve(x0, y0, X, Y, dXdt, dYdt, eps);
     toc;
