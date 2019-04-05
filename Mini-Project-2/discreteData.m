39bcc76cd1aff0cd76147569afd6f02cce332e5d
Aniket Pandey <aniketp@iitk.ac.in>
2019-04-06 01:41:59 +0530

    Project code housekeeping; Comments and Bibliography

--- Mini-Project-2/discreteData.m
+++ Mini-Project-2/discreteData.m
@@ -1,11 +1,18 @@
-function [xGrid, fGrid] = discreteData(nGrid, a, b, f)
-	xGrid = zeros(1,nGrid);
-	fGrid = zeros(1,nGrid);
-	
-	for (i = 1 : nGrid)
-		xGrid(i) = (a + b)/2 + ((b - a)/2) * (cos(pi*(i-1)/(nGrid - 1)));
-		fGrid(i) = f(xGrid(i));
-	end
-end
+% Create the interpolation points for function approximation
+%
+% Function signature: returns Chebyshev nodes
+%
+% Name: Aniket Pandey
+% Roll: 160113
+% Dept: B.S MTH
 
-        
+function [xGrid, fGrid] = discreteData(nGrid, a, b, f)
+    xGrid = zeros(1, nGrid);
+    fGrid = zeros(1, nGrid);
+    
+    for i = 1 : nGrid
+        arg = pi * ((i - 1)/(nGrid - 1));
+        xGrid(i) = (a + b)/2 + ((b - a)/2) * (cos(arg));
+        fGrid(i) = f(xGrid(i));
+    end
+end
--- Mini-Project-2/main.m
+++ Mini-Project-2/main.m
@@ -8,24 +8,25 @@ b = 10;         % the value of the right end-point of the interval
 f = @(x) ((x-a)*(b-x))^11;
 
 % approximation grid size
-nGrid = 100;            % size of the grid to be used for approximation
+nGrid = 100;    % size of the grid to be used for approximation
 [xGrid, fGrid] = discreteData(nGrid, a, b, f);
 
 % the number of test evaluation points
 nEval = 10;
-xEval = a + (b-a).*rand(1,nEval); % evaluation grid to be used for testing
+% evaluation grid to be used for testing
+xEval = a + (b - a).*rand(1, nEval);
 
 tic;
-%exact = f(xEval);
 exact = zeros(1, nEval);
-for i=1:nEval
+for i = 1 : nEval
     exact(i) = f(xEval(i));
 end
-%---------------------------------------------------
+
+%---------------------------------------------------%
 approx = approxFunction(xEval, xGrid, fGrid);
 
-maxExact = max(abs(exact)); % the max norm of f
-maxError = max(abs(approx-exact)); % the max norm of error
+maxExact = max(abs(exact));         % the max norm of f
+maxError = max(abs(approx-exact));  % the max norm of error
 toc;
 
-maxError/maxExact
+fprintf("maxError/maxExact = %e\n", maxError/maxExact);
